1489ba05a31fc5950b16ee6c0e2d1f18113b31d5
Paul Grock <paulgrock@gmail.com>
2016-01-03 15:03:35 -0800

    Move menu into separate file

---
+++ menu.js
@@ -0,0 +1,189 @@
+import dialog from 'dialog';
+import app from 'app';
+
+const menuConstructor = (win) => {
+	const menu = [
+		{
+			label: 'File',
+			submenu: [
+				{
+					label: 'Open',
+					accelerator: 'CmdOrCtrl+O',
+					click() {
+						const options = {
+							title: 'Add files to be transmformed',
+							properties: ['openFile', 'openDirectory', 'multiSelections']
+						};
+						dialog.showOpenDialog(win, options, (files) => {
+							if (files == null) {
+								return;
+							}
+							win.webContents.send('new-files', files);
+						});
+					}
+				}
+			]
+		},
+		{
+			label: 'Edit',
+			submenu: [
+				{
+					label: 'Undo',
+					accelerator: 'CmdOrCtrl+Z',
+					role: 'undo'
+				},
+				{
+					label: 'Redo',
+					accelerator: 'Shift+CmdOrCtrl+Z',
+					role: 'redo'
+				},
+				{
+					type: 'separator'
+				},
+				{
+					label: 'Cut',
+					accelerator: 'CmdOrCtrl+X',
+					role: 'cut'
+				},
+				{
+					label: 'Copy',
+					accelerator: 'CmdOrCtrl+C',
+					role: 'copy'
+				},
+				{
+					label: 'Paste',
+					accelerator: 'CmdOrCtrl+V',
+					role: 'paste'
+				},
+				{
+					label: 'Select All',
+					accelerator: 'CmdOrCtrl+A',
+					role: 'selectall'
+				}
+			]
+		},
+		{
+			label: 'View',
+			submenu: [
+				{
+					label: 'Reload',
+					accelerator: 'CmdOrCtrl+R',
+					click(item, focusedWindow) {
+						if (focusedWindow) {
+							focusedWindow.reload();
+						}
+					}
+				},
+				{
+					label: 'Toggle Full Screen',
+					accelerator: (function () {
+						if (process.platform === 'darwin') {
+							return 'Ctrl+Command+F';
+						}
+						return 'F11';
+					})(),
+					click(item, focusedWindow) {
+						if (focusedWindow) {
+							focusedWindow.setFullScreen(!focusedWindow.isFullScreen());
+						}
+					}
+				},
+				{
+					label: 'Toggle Developer Tools',
+					accelerator: (function () {
+						if (process.platform === 'darwin') {
+							return 'Alt+Command+I';
+						}
+						return 'Ctrl+Shift+I';
+					})(),
+					click(item, focusedWindow) {
+						if (focusedWindow) {
+							focusedWindow.toggleDevTools();
+						}
+					}
+				}
+			]
+		},
+		{
+			label: 'Window',
+			role: 'window',
+			submenu: [
+				{
+					label: 'Minimize',
+					accelerator: 'CmdOrCtrl+M',
+					role: 'minimize'
+				},
+				{
+					label: 'Close',
+					accelerator: 'CmdOrCtrl+W',
+					role: 'close'
+				}
+			]
+		},
+		{
+			label: 'Help',
+			role: 'help'
+		}
+	];
+
+	if (process.platform === 'darwin') {
+		const name = app.getName();
+		menu.unshift({
+			label: name,
+			submenu: [
+				{
+					label: `About ${name}`,
+					role: 'about'
+				},
+				{
+					type: 'separator'
+				},
+				{
+					label: 'Services',
+					role: 'services',
+					submenu: []
+				},
+				{
+					type: 'separator'
+				},
+				{
+					label: `Hide ${name}`,
+					accelerator: 'Command+H',
+					role: 'hide'
+				},
+				{
+					label: 'Hide Others',
+					accelerator: 'Command+Shift+H',
+					role: 'hideothers'
+				},
+				{
+					label: 'Show All',
+					role: 'unhide'
+				},
+				{
+					type: 'separator'
+				},
+				{
+					label: 'Quit',
+					accelerator: 'Command+Q',
+					click() {
+						app.quit();
+					}
+				}
+			]
+		});
+		// Window menu.
+		menu[3].submenu.push(
+			{
+				type: 'separator'
+			},
+			{
+				label: 'Bring All to Front',
+				role: 'front'
+			}
+		);
+	}
+	return menu;
+};
+
+export default menuConstructor;
